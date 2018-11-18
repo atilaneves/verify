c2e8bae6d6e1bd76b75f0ffd8aac78223b21928b
Atila Neves <atila.neves@gmail.com>
2018-11-18 22:58:14 +0100

    Equality and function call

--- source/verify/interpreter.d
+++ source/verify/interpreter.d
@@ -28,6 +28,27 @@ private extern (C++) final class UnitTestInterpreter: from!"dmd.visitor".Visitor
     alias visit = Visitor.visit;
 
     Scope* _scope;
+    Expression result;
+
+    final Expression eval(Expression expression) {
+        expression.accept(this);
+        return result;
+    }
+
+    final Expression eval(Statement statement) {
+        statement.accept(this);
+        return result;
+    }
+
+    override void visit(Expression expression) {
+        import std.conv: text;
+        throw new Exception(text("Dunno how to handle expression '", expression.tostring, "'"));
+    }
+
+    override void visit(Statement statement) {
+        import std.conv: text;
+        throw new Exception(text("Dunno how to handle statement '", statement.tostring, "'"));
+    }
 
     override void visit(UnitTestDeclaration test) {
         log("\n");
@@ -50,28 +71,61 @@ private extern (C++) final class UnitTestInterpreter: from!"dmd.visitor".Visitor
     }
 
     override void visit(AssertExp assertion) {
+        import verify.exception: TestFailure;
+        import std.conv: text;
+
         assert(_scope !is null);
 
         log("            AssertExp @ ", assertion.loc.tostring, ": '", assertion.tostring, "'");
         log("            AssertExp e1: '", assertion.e1.tostring, "'");
 
         assertion.e1.accept(this);
+
+        if(result is null)
+            throw new Exception("No result to check");
+
+        if(!result.toInteger)
+             throw new TestFailure(text("Failure: `", result.tostring, "`"));
     }
 
     // booleans are included here
     override void visit(IntegerExp expression) {
+        log("            IntegerExp: '", expression.tostring, "'");
+        result = expression;
+    }
+
+    override void visit(EqualExp expression) {
         import verify.exception: TestFailure;
         import std.conv: text;
 
-        log("            IntegerExp: '", expression.tostring, "'");
+        // TODO: need to handle both expression.op == TOK.equal and TOK.notEqual
+        log("            EqualExp: '", expression.tostring, "' op: ", expression.op);
+        log("                e1: '", expression.e1.tostring, "'  op: ", expression.e1.op);
+        log("                e2: '", expression.e2.tostring, "'  op: ", expression.e2.op);
 
-        if(!expression.toInteger)
-            throw new TestFailure(text("Failure: `", expression.tostring, "`"));
+        auto lhs = eval(expression.e1);
+        auto rhs = eval(expression.e2);
+
+        if(lhs != rhs)
+            throw new TestFailure(text("Expected: ", lhs, "  Got: ", rhs));
+    }
+
+    override void visit(CallExp expression) {
+        // TODO: function arguments
+        result = eval(expression.f.fbody);
+    }
+
+    override void visit(ReturnStatement statement) {
+        log("            Return statement: '", statement.tostring, "'");
+        result = eval(statement.exp);
     }
 }
 
 
 private auto tostring(T)(auto ref T obj) {
     import std.string: fromStringz, strip;
-    return obj.toChars.fromStringz.strip;
+    static if(__traits(compiles, obj is null))
+        return obj is null ? "null" : obj.toChars.fromStringz.strip;
+    else
+        return obj.toChars.fromStringz.strip;
 }
--- tests/ut/assertions.d
+++ tests/ut/assertions.d
@@ -48,3 +48,28 @@ import ut;
         tests[1].run.shouldThrowWithMessage!TestFailure("Failure: `0`");
     }
 }
+
+
+@("assert.literal.equals")
+@safe unittest {
+    with(const TestModule(
+             q{
+                 unittest { assert(1 == 3); }
+             }))
+    {
+        unitTests[0].run.shouldThrowWithMessage!TestFailure("Failure: `false`");
+    }
+}
+
+@("assert.literal.function.constant")
+@safe unittest {
+    with(const TestModule(
+             q{
+                 int five() { return 5; }
+                 unittest { assert(five() == 3); }
+             }))
+    {
+        unitTests[0].run.shouldThrowWithMessage!TestFailure("Expected: 5  Got: 3");
+    }
+
+}
